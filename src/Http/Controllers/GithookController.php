08da9407bbab1cce7ac799a5d5186e91b3cb5cc6
Emeka Mbah <frankemeks77@yahoo.com>
2021-10-18 02:58:34 +0100

    add controllers

---
+++ src/Http/Controllers/GithookController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Digitlimit\Githook\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Digitlimit\Githook\Events\NoPayload;
+use Digitlimit\Githook\Events\Webhook;
+
+class GithookController extends Controller
+{
+    /**
+     * Perform the git hook.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function __invoke(Request $request)
+    {
+        if (!$request->has('payload')) {
+            NoPayload::dispatch($request->all());
+            info('No payload received');
+            return;
+        }
+
+        // get payload
+        $payload = json_decode($request->payload);
+        $event = $request->header('X-GitHub-Event');
+
+        event(new Webhook($payload, $event));
+    }
+
+    /**
+     * Check authentication
+     *
+     * @param [type] $payload
+     * @return void
+     */
+    protected function authCheck()
+    {
+        $github_hash = request()->header('X-Hub-Signature');
+        $secret = config('githook.secret');
+        $payload = request()->getContent();
+        
+        $local_hash = 'sha1=' . hash_hmac('sha1', $payload, $secret, false);
+
+        return hash_equals($github_hash, $local_hash);
+    }
+}
