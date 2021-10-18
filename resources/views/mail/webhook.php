19e38d1780236d6963c9f7020ec26622c8f69d19
Emeka Mbah <frankemeks77@yahoo.com>
2021-10-18 02:57:15 +0100

    add resources

---
+++ resources/views/mail/webhook.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Webhook Event</title>
+</head>
+
+<body>
+	<div>
+		<h1>Webhook {{$type}} Event</h1>
+		<p>There is push event to repo</p>
+	</div>
+</body>
+</html>
